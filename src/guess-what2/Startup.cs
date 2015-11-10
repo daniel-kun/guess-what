e2a1b7a904fd9e38e856c7660930df41aa88a5af
Daniel Albuschat <d.albuschat@gmail.com>
2015-11-10 20:30:46 +0100

    Added bootstrap and jquery via bower, gulp and npm - I used a great tutorial from http://shellmonger.com/2015/02/28/adding-bootstrap-to-your-empty-asp-net-mvc6-project/

--- src/guess-what2/Startup.cs
+++ src/guess-what2/Startup.cs
@@ -9,14 +9,12 @@ namespace guess_what2
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
