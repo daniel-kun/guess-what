8f74143360a46dcaee8def41ea01ba3cbb5499a4
Daniel Albuschat <d.albuschat@gmail.com>
2015-11-13 19:51:35 +0100

    Added server-side Application Insights

--- src/guess-what2/Startup.cs
+++ src/guess-what2/Startup.cs
@@ -1,20 +1,46 @@
 ﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Runtime;
 
 namespace guess_what2
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddApplicationInsightsTelemetry(Configuration);
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseApplicationInsightsRequestTelemetry();
+
+            // Add Application Insights to the request pipeline to track HTTP request telemetry data.
             app.UseMvcWithDefaultRoute();
+            // Track data about exceptions from the application. Should be configured after all error handling middleware in the request pipeline.
+            app.UseApplicationInsightsExceptionTelemetry();
+        }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+
+            if (env.IsEnvironment("Development"))
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            Configuration = builder.Build();
         }
     }
 }
