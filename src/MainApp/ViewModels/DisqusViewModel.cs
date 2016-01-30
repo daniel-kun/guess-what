7c5cc1839210a80d4e4043fe50cc9398089f9ade
Daniel Albuschat <d.albuschat@gmail.com>
2016-01-30 21:32:49 +0100

    Disqus-integration! (yay, that was easy) Added a dev-mode favicon

---
+++ src/MainApp/ViewModels/DisqusViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Io.GuessWhat.MainApp.ViewModels
+{
+    /**
+    The DisqusViewModel includes config variables that are required to
+    integrate Disqus on a page.
+    **/
+    public class DisqusViewModel
+    {
+        public DisqusViewModel(string url, string id)
+        {
+            Url = url;
+            Id = id;
+        }
+
+        /**
+        The canonical URL of the page that Disqus is being integrated in.
+        **/
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        /**
+        A unique id for the site. 
+        **/
+        public string Id
+        {
+            get;
+            set;
+        }
+    }
+}
