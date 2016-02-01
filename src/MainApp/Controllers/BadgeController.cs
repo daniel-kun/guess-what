ffa080288de2594c90cc7201ac930c4748769218
Daniel Albuschat <d.albuschat@gmail.com>
2016-02-01 12:57:04 +0100

    The QA badge is now accessible via .png and .svg direct links

---
+++ src/MainApp/Controllers/BadgeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Mvc;
+using Io.GuessWhat.MainApp.Repositories;
+using Io.GuessWhat.MainApp.ViewModels;
+using System.Net;
+using Io.GuessWhat.Tools.Web;
+
+namespace Io.GuessWhat.MainApp.Controllers
+{
+    /**
+    The BadgeController provides checklist result badges in .svg and .png format.
+    **/
+    [Route("badge")]
+    public class BadgeController : Controller
+    {
+        public BadgeController(IChecklistRepository checklistRepository)
+        {
+            mChecklistRepository = checklistRepository;
+        }
+
+        /**
+        Delivers a badge in .png format
+        **/
+        [Route("{id}.png")]
+        public IActionResult Index(string id)
+        {
+            var svgRequest = WebRequest.CreateHttp("http://localhost:54730/badge/K1HsV7Jda0mTRYR-Y6BTOQ.svg");
+            svgRequest.Method = "GET";
+            return new SvgRenderer(svgRequest.GetResponse().GetResponseStream());
+        }
+
+        /**
+        Delivers a badge in .svg format
+        **/
+        [Route("{id}.svg")]
+        public IActionResult Svg(string id)
+        {
+            var resultModel = mChecklistRepository.LoadChecklistResultModel(id);
+            if (resultModel == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var viewModel = ChecklistResultViewModel.FromResult(resultModel);
+                return View(viewModel);
+            }
+        }
+
+        private IChecklistRepository mChecklistRepository;
+
+    }
+}
---
+++ src/Tools/Graphics/SvgToPng.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Io.GuessWhat.Tools.Graphics
+{
+    public class SvgToPng
+    {
+        public static Task ConvertSvgToPng(Stream input, Stream output)
+        {
+            Svg.SvgDocument doc = Svg.SvgDocument.Open<Svg.SvgDocument>(input);
+            using (Bitmap bmp = doc.Draw())
+            {
+                using (var mem = new MemoryStream())
+                {
+                    bmp.Save(mem, ImageFormat.Png);
+                    mem.Position = 0;
+                    return mem.CopyToAsync(output);
+                }
+            }
+        }
+    }
+}
