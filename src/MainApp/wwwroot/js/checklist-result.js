eb33d4ea2c719f3640dd3c7aec5543ce5af34ebc
Daniel Albuschat <d.albuschat@gmail.com>
2016-02-19 10:28:51 +0100

    New share platform: Redmine Added "copy code for redmine" functionality Added a "copied to clipboard" notice

--- src/MainApp/wwwroot/js/checklist-result.js
+++ src/MainApp/wwwroot/js/checklist-result.js
@@ -4,25 +4,41 @@ $(function () {
 
     $("#jira-badgecode-copy").click(function (e) {
         e.preventDefault();
-        $("#github-badgecode").addClass("hidden");
-        $("#url-badgecode").addClass("hidden");
-        $("#jira-badgecode").removeClass("hidden");
+        $("#jira-badgecode"     ).removeClass("hidden");
+        $("#github-badgecode"   ).addClass("hidden");
+        $("#redmine-badgecode"  ).addClass("hidden");
+        $("#url-badgecode"      ).addClass("hidden");
         copyUrlToClipboard($("#jira-badgecode"));
+        $("#badgecode-copy-notice").removeClass("hidden");
     });
 
     $("#github-badgecode-copy").click(function (e) {
         e.preventDefault();
-        $("#github-badgecode").removeClass("hidden");
-        $("#url-badgecode").addClass("hidden");
-        $("#jira-badgecode").addClass("hidden");
+        $("#jira-badgecode"     ).addClass("hidden");
+        $("#github-badgecode"   ).removeClass("hidden");
+        $("#redmine-badgecode"  ).addClass("hidden");
+        $("#url-badgecode"      ).addClass("hidden");
         copyUrlToClipboard($("#github-badgecode"));
+        $("#badgecode-copy-notice").removeClass("hidden");
+    });
+
+    $("#redmine-badgecode-copy").click(function (e) {
+        e.preventDefault();
+        $("#jira-badgecode"     ).addClass("hidden");
+        $("#github-badgecode"   ).addClass("hidden");
+        $("#redmine-badgecode"  ).removeClass("hidden");
+        $("#url-badgecode"      ).addClass("hidden");
+        copyUrlToClipboard($("#redmine-badgecode"));
+        $("#badgecode-copy-notice").removeClass("hidden");
     });
 
     $("#url-badgecode-copy").click(function (e) {
         e.preventDefault();
-        $("#github-badgecode").addClass("hidden");
-        $("#jira-badgecode").addClass("hidden");
-        $("#url-badgecode").removeClass("hidden");
+        $("#jira-badgecode"     ).addClass("hidden");
+        $("#github-badgecode"   ).addClass("hidden");
+        $("#redmine-badgecode"  ).addClass("hidden");
+        $("#url-badgecode"      ).removeClass("hidden");
         copyUrlToClipboard($("#url-badgecode"));
+        $("#badgecode-copy-notice").removeClass("hidden");
     });
 })
